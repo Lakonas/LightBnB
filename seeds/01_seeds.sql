0e13e971115f53a0bb75d24b4022f9567305b294
Bill Katsoulis <bill.katsoulis@rogers.com>
2025-01-31 18:30:06 -0500

    More corrections

--- seeds/01_seeds.sql
+++ seeds/01_seeds.sql
@@ -9,12 +9,12 @@ VALUES
 (1, 'My Cottage', 'not too bad.', 'thumbnail_cottage.jpg', 'cover_cottage.jpg', 150, 2, 2, 1, 'USA', '42 Maple St', 'Springfield', 'IL', '62704', true),
 ...
 
-INSERT INTO reservations (id,start_date, end_date, property_id, guest_id)
+INSERT INTO reservations (start_date, end_date, property_id, guest_id)
 VALUES (1, '2018-09-11','2018-09-26',1,3 ),
 (1,'2019-01-04', '2021-10-14',5,6),
 (1,'2019-01-09', '2021-10-15',7,2);
 
 INSERT INTO property_reviews (guest_id, property_id, reservation_id, rating, message)
-VALUES (1,2,3),
+VALUES (1,2,3, 'message'),
 (2,3,4, 'message'),
 (3,4,5 'message'),
