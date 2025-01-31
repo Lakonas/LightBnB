59f85d2ae087a9ecbbeb80b4bd523dd07be1e3b7
Bill Katsoulis <bill.katsoulis@rogers.com>
2025-01-31 18:15:15 -0500

    Applied larry ai corrections

--- seeds/01_seeds.sql
+++ seeds/01_seeds.sql
@@ -1,19 +1,20 @@
-INSERT INTO users (id,name, email, password)
-VALUES (1, 'Basil', 'bill@bill.com', "$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u"),
-(2, 'Eleni', 'eleni@eleni.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u'),
-{3, 'Aristotle', 'ari@squareofopposition.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u'},
+INSERT INTO users (name, email, password)
+VALUES 
+('Basil', 'bill@bill.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u'),
+('Eleni', 'eleni@eleni.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u'),
+('Aristotle', 'ari@squareofopposition.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u');
 
-INSERT INTO properties (id, owner_id, title, thumbnail_photo, cover_photo_url, cost_per_night, parking_spaces, number_of_bathrooms, number_of_bedrooms, country, street, city, province, post_code, active);
+INSERT INTO properties (id, owner_id, title,)
 VALUES(1,5, 'mR'),
 (2,2,'MRS'),
 (3,3,'TEACHER');
 
-INSERT INTO reservations (1d,start_date, end_date)
-VALUES (1, '2018-09-11','2018-09-26' ),
-(1,'2019-01-04', '2021-10-14'),
-(1,'2019-01-04', 2021-10-14 );
+INSERT INTO reservations (id,start_date, end_date, property_id, guest_id)
+VALUES (1, '2018-09-11','2018-09-26',1,3 ),
+(1,'2019-01-04', '2021-10-14',2,3),
+(1,'2019-01-04', '2021-10-14',2,2);
 
-INSERT INTO property_reviews ( id, guest_id, property_id)
+INSERT INTO property_reviews (guest_id, property_idreservation_id, rating, message)
 VALUES (1,2,3),
-(2,3,4),
-(3,4,5);
+(2,3,4, 'message'),
+(3,4,5 'message');
